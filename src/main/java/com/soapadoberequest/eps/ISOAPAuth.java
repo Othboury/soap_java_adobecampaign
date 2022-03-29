29bf288a1fcedb495a87b9764c7a28247be035a4
Othmane BOURY <othboury@publicisgroupe.net>
2022-03-29 15:09:11 +0200

    Add gitignore

--- src/main/java/com/soapadoberequest/eps/ISOAPAuth.java
+++ src/main/java/com/soapadoberequest/eps/ISOAPAuth.java
@@ -6,13 +6,6 @@ import java.util.ArrayList;
 
 public interface ISOAPAuth {
     public ArrayList<Node> postSOAPAUTH();
-    /*public void postSOAPInsert(String firstname, String lastname, String email, String sessionToken,
-                               String securityToken);
-    public void postSOAPSelect(String email, String sessionToken, String securityToken);
-    public void postSOAPStartWorkflow(String workflowId, String sessionToken, String securityToken);*/
     public void postSOAPSubscribe(Recipient recipient, String serviceName, String sessionToken, String securityToken);
-    /*public void postSOAPPostEvent(String workFlowId, String activity, ArrayList<String> vars ,ArrayList<String> param,
-                                  String sessionToken, String securityToken);
-    public void postSOAPWrite(Recipient recipient, String sessionToken, String securityToken);
-    public void postSOAPKillWorkflow(String workFlowId, String sessionToken, String securityToken);*/
+
 }
