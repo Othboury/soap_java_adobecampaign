b65d6cc6509ae7b037d5d15ed1ca48b4940cd4d5
Othmane BOURY <othboury@publicisgroupe.net>
2022-03-30 17:16:18 +0200

    Add JavaDoc to HttpClient Interface

--- src/main/java/com/soapadoberequest/eps/IHttpClientClass.java
+++ src/main/java/com/soapadoberequest/eps/IHttpClientClass.java
@@ -4,7 +4,29 @@ import org.apache.http.HttpEntity;
 
 import java.io.IOException;
 
+/**
+ * This class is an interface containing the methods used in the class HttpClientClass
+ */
 public interface IHttpClientClass {
-    public HttpEntity httpClientCall(String soapBody, String SOAPAction, String sessionToken, String securityToken) throws IOException;
+
+    /**
+     * This method is used to launch HTTPClient for different SOAP requests
+     * @param soapBody
+     * @param SOAPAction
+     * @param sessionToken
+     * @param securityToken
+     * @return Response Entity
+     * @throws IOException
+     */
+    public HttpEntity httpClientCall(String soapBody, String SOAPAction, String sessionToken, String securityToken)
+            throws IOException;
+
+    /**
+     * This method is used to launch HTTPClient to logon in order to get session and security tokens
+     *
+     * @param soapBody
+     * @return response Entity
+     * @throws IOException
+     */
     public HttpEntity httpClientLogon(String soapBody) throws IOException;
 }
