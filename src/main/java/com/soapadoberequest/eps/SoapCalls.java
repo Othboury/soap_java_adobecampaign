b3143a3387373b46df2f366d39d58b4213aaf1af
Othmane BOURY <othboury@publicisgroupe.net>
2022-03-30 13:36:17 +0200

    SonarLint plugin added to correct bugs

--- src/main/java/com/soapadoberequest/eps/SoapCalls.java
+++ src/main/java/com/soapadoberequest/eps/SoapCalls.java
@@ -1,9 +1,10 @@
 package com.soapadoberequest.eps;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.w3c.dom.Node;
-
-import java.io.File;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * The main class
@@ -11,7 +12,9 @@ import java.util.ArrayList;
 public class SoapCalls {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("START SOAP REQUESTS...");
+        Dotenv dotenv=Dotenv.configure().ignoreIfMissing().ignoreIfMalformed().load();
+        Logger logger = Logger.getLogger("logger");
+        logger.log(Level.INFO, "START SOAP REQUESTS...");
 
         //Declare the soap objects for each functionality
         SoapAuth soapAuth = new SoapAuth();
@@ -19,7 +22,7 @@ public class SoapCalls {
         SoapDelivery soapDelivery = new SoapDelivery();
         SoapWorkflow soapWorkflow = new SoapWorkflow();
 
-        System.out.println("********AUTHENTICATE TO A SESSION********");
+        logger.log(Level.INFO,"********AUTHENTICATE TO A SESSION********");
         ArrayList<Node> tokens =  soapAuth.postSOAPAUTH();
         Recipient recipient = new Recipient("Fatim zahra","Bachri","fatbac@gmail.com");
 
@@ -29,57 +32,57 @@ public class SoapCalls {
         varName.add("email");
         varValue.add("othboury@gmail.com");
 
-        /*System.out.println("-------QUERY CALLS-------");
-        System.out.println("********INSERT NEW RECIPIENT INTO DB********");
+        logger.log(Level.INFO,"-------QUERY CALLS-------");
+        logger.log(Level.INFO,"********INSERT NEW RECIPIENT INTO DB********");
         soapQuery.postSOAPInsert("Messi", "loko", "treiue@xyz.com",
                 tokens.get(0).getTextContent(), tokens.get(1).getTextContent());
-        System.out.println("********SELECT RECIPIENT FROM DB********");
+        logger.log(Level.INFO,"********SELECT RECIPIENT FROM DB********");
         soapQuery.postSOAPSelect("othboury@gmail.com",tokens.get(0).getTextContent(),
                 tokens.get(1).getTextContent());
-        System.out.println("********SUBSCRIBE EXISTING RECIPIENT TO AN EXISTING SERVICE********");
+        logger.log(Level.INFO,"********SUBSCRIBE EXISTING RECIPIENT TO AN EXISTING SERVICE********");
         soapAuth.postSOAPSubscribe(recipient, "SVC1",tokens.get(0).getTextContent(),
                 tokens.get(1).getTextContent());
-        System.out.println("********WRITE A NEW RECIPIENT********");
-        String filename = "C:\\Users\\othboury\\Desktop\\Files\\recipient.csv";
+        logger.log(Level.INFO,"********WRITE A NEW RECIPIENT********");
+        String filename = dotenv.get("FILE_LOCATION");
         soapQuery.postSOAPWriteCollection( filename, tokens.get(0).getTextContent(),
-                tokens.get(1).getTextContent());*/
+                tokens.get(1).getTextContent());
 
-        System.out.println("-------WORKFLOW CALLS-------");
-        /*System.out.println("********START AN EXISTING WORKFLOW********");
+        logger.log(Level.INFO,"-------WORKFLOW CALLS-------");
+        logger.log(Level.INFO,"********START AN EXISTING WORKFLOW********");
         soapWorkflow.postSOAPStartWorkflow("WKF33",tokens.get(0).getTextContent(),
                 tokens.get(1).getTextContent());
-        System.out.println("********TRIGGER A WORKFLOW START FROM SIGNAL********");
+        logger.log(Level.INFO,"********TRIGGER A WORKFLOW START FROM SIGNAL********");
         soapWorkflow.postSOAPPostEvent("WKF12", "signal", varName,varValue,
                 tokens.get(0).getTextContent(), tokens.get(1).getTextContent());
-        System.out.println("********KILL A WORKFLOW********");
+        logger.log(Level.INFO,"********KILL A WORKFLOW********");
         soapWorkflow.postSOAPKillWorkflow("WKF8",tokens.get(0).getTextContent(),
-                tokens.get(1).getTextContent() );*/
-        System.out.println("********PAUSE A WORKFLOW********");
+                tokens.get(1).getTextContent() );
+        logger.log(Level.INFO,"********PAUSE A WORKFLOW********");
         soapWorkflow.postSOAPPauseWorkflow("WKF33",tokens.get(0).getTextContent(),
                 tokens.get(1).getTextContent() );
-        /*System.out.println("********WAKE UP A WORKFLOW********");
+        logger.log(Level.INFO,"********WAKE UP A WORKFLOW********");
         soapWorkflow.postSOAPWakeUpWorkflow("WKF8",tokens.get(0).getTextContent(),
                 tokens.get(1).getTextContent() );
-        System.out.println("********START A WORKFLOW WITH PARAMETERS********");
+        logger.log(Level.INFO,"********START A WORKFLOW WITH PARAMETERS********");
         soapWorkflow.postSOAPStartWithParams("WKF13", varName, varValue, tokens.get(0).getTextContent(),
                 tokens.get(1).getTextContent() );
         soapWorkflow.postSOAPWorkflowLogs("16873",tokens.get(0).getTextContent(),
-                tokens.get(1).getTextContent());*/
-
-        System.out.println("-------DELIVERY CALLS-------");
-       /* System.out.println("********SELECT A DELIVERY********");
-        soapDelivery.postSOAPSelectDelivery("DM33",tokens.get(0).getTextContent(),
-                tokens.get(1).getTextContent());*/
-        /*soapDelivery.postSOAPCreateWithTemplate("DM7",varName, varValue,"",
-                tokens.get(0).getTextContent(), tokens.get(1).getTextContent());*/
-       /* soapDelivery.postSOAPPrepareAndStart("DM40",tokens.get(0).getTextContent(),
                 tokens.get(1).getTextContent());
-        soapDelivery.postSOAPPrepareTarget(114886,tokens.get(0).getTextContent(),
-                tokens.get(1).getTextContent());/
+
+        logger.log(Level.INFO,"-------DELIVERY CALLS-------");
+        logger.log(Level.INFO,"********SELECT A DELIVERY********");
+        soapDelivery.postSOAPSelectDelivery("DM33",tokens.get(0).getTextContent(),
+                tokens.get(1).getTextContent());
+        soapDelivery.postSOAPCreateWithTemplate("DM7",varName, varValue,"",
+                tokens.get(0).getTextContent(), tokens.get(1).getTextContent());
+        soapDelivery.postSOAPPrepareAndStart("DM40",tokens.get(0).getTextContent(),
+                tokens.get(1).getTextContent());
+        soapDelivery.postSOAPPrepareTarget("DM40",tokens.get(0).getTextContent(),
+                tokens.get(1).getTextContent());
         soapDelivery.postSOAPPrepareMessage("DM40",tokens.get(0).getTextContent(),
                 tokens.get(1).getTextContent());
         soapDelivery.postSOAPSubmitDelivery("DM7",tokens.get(0).getTextContent(),
-                tokens.get(1).getTextContent());*/
-        System.out.println("END SOAP REQUESTS...");
+                tokens.get(1).getTextContent());
+        logger.log(Level.INFO, "END SOAP REQUESTS...");
     }
 }
